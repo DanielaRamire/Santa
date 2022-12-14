d0b9475a742a571cc44bff0dfffc13757f3f30ca
DanielaRamire <danielaramirez2003b@gmail.com>
2022-12-14 17:12:33 -0500

    prueba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './pages/navbar/navbar.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { HomeComponent } from './pages/components/home/home.component';
+import { MenuComponent } from './pages/components/menu/menu.component';
+import { AboutComponent } from './pages/components/about/about.component';
+import { ContactComponent } from './pages/components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    MenuComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
